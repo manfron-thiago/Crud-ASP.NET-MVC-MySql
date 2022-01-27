53715c0ed78b1b4a3944f13e05634bd46feea54d
manfron-thiago <thiago.mfrn@gmail.com>
2022-01-27 00:42:52 -0300

    MySql Migration

---
+++ WebCrud/Data/WebCrudContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebCrud.Models;
+
+namespace WebCrud.Data.Models
+{
+    public class WebCrudContext : DbContext
+    {
+        public WebCrudContext (DbContextOptions<WebCrudContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebCrud.Models.Department> Department { get; set; }
+    }
+}
