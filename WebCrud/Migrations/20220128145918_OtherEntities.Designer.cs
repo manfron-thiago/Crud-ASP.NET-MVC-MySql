902694d131c3f105d188f98022b871d884ef62e1
manfron-thiago <thiago.mfrn@gmail.com>
2022-01-28 12:14:15 -0300

    Other Entities and Second Migration

--- WebCrud/Migrations/20220128145918_OtherEntities.Designer.cs
+++ WebCrud/Migrations/20220128145918_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebCrud.Data.Models;
 
 namespace WebCrud.Migrations
 {
     [DbContext(typeof(WebCrudContext))]
-    partial class WebCrudContextModelSnapshot : ModelSnapshot
+    [Migration("20220128145918_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
