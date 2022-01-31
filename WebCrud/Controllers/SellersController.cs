541d1699788b802429476cbd2db78973e854c4c1
manfron-thiago <thiago.mfrn@gmail.com>
2022-01-31 20:50:14 -0300

    Simple Create Form

--- WebCrud/Controllers/SellersController.cs
+++ WebCrud/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebCrud.Models;
 using WebCrud.Services;
 
 namespace WebCrud.Controllers
@@ -19,6 +20,18 @@ namespace WebCrud.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
