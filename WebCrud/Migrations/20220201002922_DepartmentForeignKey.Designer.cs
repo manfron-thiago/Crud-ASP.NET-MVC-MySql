05d75c90caa64fc1c188260a43b2eddb1db7d31d
manfron-thiago <thiago.mfrn@gmail.com>
2022-01-31 21:35:53 -0300

    Foreign Key Not Null

--- WebCrud/Migrations/20220201002922_DepartmentForeignKey.Designer.cs
+++ WebCrud/Migrations/20220201002922_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebCrud.Data.Models;
 
 namespace WebCrud.Migrations
 {
     [DbContext(typeof(WebCrudContext))]
-    partial class WebCrudContextModelSnapshot : ModelSnapshot
+    [Migration("20220201002922_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
