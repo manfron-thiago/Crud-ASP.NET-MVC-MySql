44b13ef19b2ae5f6c6a7a3dc709ab8ea22fa702b
manfron-thiago <thiago.mfrn@gmail.com>
2022-01-31 22:41:54 -0300

    Seller Details and Eager Loading

--- WebCrud/Services/SellerService.cs
+++ WebCrud/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebCrud.Data.Models;
 using WebCrud.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebCrud.Services
 {
@@ -26,7 +27,7 @@ namespace WebCrud.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
