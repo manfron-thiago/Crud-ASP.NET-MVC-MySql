7cb87b6605093a44f23fa11d9aa667be121f67c9
manfron-thiago <thiago.mfrn@gmail.com>
2022-02-04 10:54:28 -0300

    App Locale, Number and Date Formatting

--- WebCrud/Startup.cs
+++ WebCrud/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.EntityFrameworkCore;
 using WebCrud.Data.Models;
 using WebCrud.Data;
 using WebCrud.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace WebCrud
 {
@@ -50,6 +52,16 @@ namespace WebCrud
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
